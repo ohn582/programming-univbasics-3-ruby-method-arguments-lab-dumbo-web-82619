cafe1db322aa7583fd45599dcf30a997dddc9a9d
Neal JIn Oh <no4259@gmail.com>
2019-08-14 14:47:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -24,7 +24,7 @@ introduction_with_language("Josh", "React");
 #introduction_with_language_optional
 
 
-def introduction_with_language_optional(name, language)
+def introduction_with_language_optional(name, language = "Ruby")
   
   puts "Hi, my name is #{name} and I am learning to program in Ruby.\n";
 
